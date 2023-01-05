edc6bc206fd95b164ac146335b840abadc372342
jonatangroncode <114836936+jonatangroncode@users.noreply.github.com>
2023-01-05 01:19:15 +0100

    Initial Commit

---
+++ GCD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by ITHS on 2023-01-05.
+//  Copyright © 2023 ITHS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
